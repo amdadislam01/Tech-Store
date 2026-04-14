c3b205da34bb79afcc58ed5c6b45bf241588cbdb
amdadisalm01 <amdadislam733@gmail.com>
2026-04-14 23:45:15 +0600

    MODIFIED: products API

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import connectDB from "@/lib/db";
 import Product from "@/models/Product";
 
@@ -30,6 +30,7 @@ export async function GET(req: Request) {
 
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
+      .populate("category")
       .sort(sortOption)
       .skip(skip)
       .limit(limit);
