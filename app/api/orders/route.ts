6641e96321c93c563df164df6a1f521341ec38e9
amdadisalm01 <amdadislam733@gmail.com>
2026-04-14 23:45:15 +0600

    MODIFIED: orders API

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 import Order from "@/models/Order";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function POST(req: Request) {
   try {
